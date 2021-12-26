10139d42424e16f21507b156a3d088a53f0d84ec
km6293 <alstn7616@naver.com>
2021-12-27 00:53:28 +0900

    css, html, for , array, function

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
 
-
-  <div>
-    <a></a>
+  <div class="menu">
+    <a v-for="a in menu" :key="a">{{a}}</a>
   </div>
-
-  <img alt="Vue logo" src="./assets/logo.png">
-  <div>
-    <h4>{{products[0]}} 원룸</h4>
-    <p>50 만원</p>
-  </div>
-  <div>
-    <h4>{{products[1]}} 원룸</h4>
-    <p>60 만원</p>
-  </div>
-  <div>
-    <h4>{{products[2]}} 원룸</h4>
+<!-- 
+  <div v-for="(a,i) in products" :key="i">
+    <h4>{{a}}</h4>
     <p>70 만원</p>
+  </div> 
+-->
+  
+  <div>
+    <h4>{{products[0]}}</h4>
+    <p>50 만원</p>
+    <button @click="increase">허위매물신고</button> 
+    <span>신고수 : {{declarations[0]}}</span>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>60 만원</p>
+    <button @click="increase">허위매물신고</button> 
+    <span>신고수 : {{declarations[2]}}</span>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>70 만원</p>
+    <button @click="increase">허위매물신고</button> 
+    <span>신고수 : {{declarations[4]}}</span>
   </div>
 
 </template>
@@ -27,7 +37,14 @@ export default {
   name: 'App',
   data(){
     return{
+      declarations : [0,0,0],
+      menu : ['Home', 'Shop', 'About'],
       products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
+  methods: {
+    increase(){
+      this.declarations += 1
     }
   },
   components: {
@@ -42,6 +59,17 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu{
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;  
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
+}
+
 </style>
