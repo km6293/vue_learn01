b8c8f3a91151a9f1d17fcbb4375bbd9692845a94
km6293 <alstn7616@naver.com>
2021-12-29 00:20:44 +0900

    v-model

--- src/Modal.vue
+++ src/Modal.vue
@@ -4,7 +4,9 @@
       <img :src="onerooms[clickNum].image" class="room-img">
       <h4>{{onerooms[clickNum].title}}</h4>
       <p>{{onerooms[clickNum].content}}</p>
-      <p>{{onerooms[clickNum].price}} 원</p>
+      <!-- v-model : 여기에 입력한 데이터를 밑에 저장. ex)input 중 select, textarea 등-->
+      <input v-model.number="month">
+      <p> {{month}} 개월 선택함 : {{onerooms[clickNum].price * month}} 원</p>
       <button @click="$emit('closeModal')">닫기</button>
     </div>
   </div>
@@ -13,6 +15,11 @@
 <script>
 export default {
   name: 'Modal',
+  data(){
+    return {
+      month : 1,
+    }
+  },
   props : {
     onerooms : Array,
     clickNum : Number,
